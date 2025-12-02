c634894a975f12ea91031cee86b68675a7d8a971
Alex-Solod <solodyhin_2@icloud.com>
2025-12-02 23:24:28 +0200

    First commit

---
+++ src/main/java/mate/academy/springbootintrohw01/SpringBootIntroHw01Application.java
@@ -0,0 +1,13 @@
+package mate.academy.springbootintrohw01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntroHw01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIntroHw01Application.class, args);
+    }
+
+}
---
+++ src/test/java/mate/academy/springbootintrohw01/SpringBootIntroHw01ApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.springbootintrohw01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntroHw01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
